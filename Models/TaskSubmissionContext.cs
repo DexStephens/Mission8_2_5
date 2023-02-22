6ad100470927192ed959ad36a70d2a7ca51eb851
Jake Evans <ejake370@byu.edu>
2023-02-22 11:33:28 -0700

    Fixed some model errors

--- Models/TaskSubmissionContext.cs
+++ Models/TaskSubmissionContext.cs
@@ -21,12 +21,36 @@ namespace Mission8_2_5.Models
         protected override void OnModelCreating(ModelBuilder mb)
         {
             mb.Entity<Category>().HasData(
-                new Category { CategoryId = 1, categoryName = "Urgent, Important"},
-                new Category { CategoryId = 2, categoryName = "Not Urgent, Important"},
-                new Category { CategoryId = 3, categoryName = "Urgent, Not Important" },
-                new Category { CategoryId = 4, categoryName = "Not Urgent, Not Important"}
-                
+                new Category { CategoryId = 1, categoryName = "Home" },
+                new Category { CategoryId = 2, categoryName = "School" },
+                new Category { CategoryId = 3, categoryName = "Work" },
+                new Category { CategoryId = 4, categoryName = "Church" }
+
+
+                );
+            mb.Entity<Task>().HasData(
+                new Task 
+                { 
+                    TaskId = 1, 
+                    CategoryId = 1,
+
+                    task = "Apply for internships",
+                    dueDate = "2023-01-01",
+                    quadrant = 3,
+                    completed = false
+                },
+
+                new Task
+                {
+                    TaskId = 2,
+                    CategoryId = 2,
+
+                    task = "Finish Mission 8",
+                    dueDate = "2023-02-24",
+                    quadrant = 2,
+                    completed = false
+                }
                 );
         }
     }
-}
+}s
